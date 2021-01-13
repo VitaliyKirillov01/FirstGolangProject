fb9add9ef8d12b3cd33475ae80c72d5cccee151e
Vitaliy Kirilov <vitaliy.kirillov.01@gmail.com>
2021-01-13 22:48:12 +0200

    Task to arrays

--- hello.go
+++ hello.go
@@ -2,6 +2,24 @@ package main
 
 import "fmt"
 
+var x = []int{
+	48, 96, 86, 68,
+	57, 82, 63, 70,
+	37, 34, 83, 27,
+	19, 97, 9, 17,
+}
+
+func minimum(x []int) int {
+	minEl := 100
+	for _, value := range x {
+		if value < minEl {
+			minEl = value
+		}
+	}
+	return minEl
+}
+
 func main() {
 	fmt.Println("Hello, NIX Education!")
+	fmt.Println(minimum(x))
 }
